6f224226bfcb440e04d78b6c03907b30fffa0d6f
Yajana-Merahkee <31155354+Yajan@users.noreply.github.com>
2019-09-13 18:25:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { NativeModules } from "react-native";
+import {requireNativeComponent} from 'react-native';
 
 module.exports = NativeModules.RNAudio;
+module.exports.ProgressBar = requireNativeComponent('SeekBar');
