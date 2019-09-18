90d0f7a4d0bccc0c224e1286df64755aaf869769
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2019-09-18 21:58:28 +0530

    Update ProgressBar.js

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,59 +1,25 @@
 import {requireNativeComponent} from 'react-native';
 import { NativeModules, StyleSheet } from "react-native";
 import React from 'react';
-
 const SeekBar = requireNativeComponent('SeekBar');
 const RNAudio = NativeModules.RNAudio;
 
 
 class ProgressBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: "init"
-    };
-  }
 
-   static getDerivedStateFromProps(props, state) {
-      if (props.status != state.status) {
-        return {
-          status: props.status
-        };
-      }
-      return null;
-    }
 	componentDidMount() {
 	    RNAudio.init();
 
   	}
 
-  	componentDidUpdate(prevProps) {
-      // Typical usage (don't forget to compare props):
-      if (this.props.status !== prevProps.status) {
-        if(this.state.status == "playing"){
-          try{
+  	componentWillUnmount(){
+  	    RNAudio.terminate();
+  	}
 
-               this._interval = setInterval(() => {
-                  RNAudio.update();
-              }, 1000);
-          }catch(error){
-              console.log("ProgressBar: "+error);
-          }
-      }else{
-        try{
-            clearInterval(this._interval);
-        }catch(error){
-        	console.log(error);
-        }
-      }
-      }
-    }
 
 
 	render(){
-		return (
-			<SeekBar style={this.props.style ? this.props.style : styles.bar} />
-			)
+		return <SeekBar style={this.props.style ? this.props.style : styles.bar} />	
 	}
 }
 
