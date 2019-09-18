d3375bd5e64c4d9b129c70d0660f0ab052507758
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2019-09-18 14:40:14 +0530

    Update MediaPlayerService.java

--- android/src/main/java/com/yajananrao/trackplayer/MediaPlayerService.java
+++ android/src/main/java/com/yajananrao/trackplayer/MediaPlayerService.java
@@ -14,11 +14,15 @@ import android.media.AudioAttributes;
 import android.media.AudioFocusRequest;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+
 import android.net.Uri;
 import android.net.wifi.WifiManager;
+
 import android.os.Build;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.Bundle;
+
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaSessionCompat;
