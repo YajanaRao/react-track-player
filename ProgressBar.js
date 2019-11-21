8628bb9b5081ffbc70f4317cdc3be592197c3086
YajanaRao <yajananrao@gmail.com>
2019-11-21 19:38:41 +0530

    updated  issue with progress bar

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,9 +1,18 @@
 import { requireNativeComponent } from "react-native";
-import { StyleSheet } from "react-native";
+import { NativeModules, StyleSheet } from "react-native";
 import React from "react";
 const SeekBar = requireNativeComponent("SeekBar");
+const MediaPlayer = NativeModules.MediaPlayer;
 
 class ProgressBar extends React.Component {
+  componentDidMount() {
+    MediaPlayer.init();
+  }
+
+  componentWillUnmount() {
+    MediaPlayer.terminate();
+  }
+
   render() {
     return <SeekBar style={this.props.style ? this.props.style : styles.bar} />;
   }
